e08a9a04a6585511e7ab789380ebef42f4c7e576
Martin Rovere <martinr@qb9.net>
2019-08-27 15:33:17 -0300

    tutorial up to JSON Files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+
+const server = app.listen(7000, () =>{
+    console.log(`Express running → PORT ${server.address().port}`);
+})
+
+
+app.get('/', (req, res) => {
+    res.render("index", {title: "Homepage"});
+});
+
+
